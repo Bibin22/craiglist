9ae184c8f08a3c6a242476a93a39f160e9f74a87
bibin22 <bibinjoy82@gmail.com>
2020-12-28 11:53:06 +0530

    edit

--- craig_list/settings.py
+++ craig_list/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6c#km*4&*#+4wa)znc1*hb68z8tuyzi&djc&(^=q&w)%u&h92w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crglst.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
